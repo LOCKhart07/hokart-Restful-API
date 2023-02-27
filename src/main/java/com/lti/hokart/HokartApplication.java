e1e70b9121776811630f99449ffac731c5f15208
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-02-27 09:18:23 +0530

    Initial commit

---
+++ src/main/java/com/lti/hokart/HokartApplication.java
@@ -0,0 +1,13 @@
+package com.lti.hokart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HokartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HokartApplication.class, args);
+	}
+
+}
